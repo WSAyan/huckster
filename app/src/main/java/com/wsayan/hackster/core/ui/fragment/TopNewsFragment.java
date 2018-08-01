aeb81487707402dcb02dd2d7e9e3e99fcfdc36ee
wsayan <wsayan28@gmail.com>
2018-08-01 19:48:36 +0600

    progressbar position fixed

--- app/src/main/java/com/wsayan/hackster/core/ui/fragment/TopNewsFragment.java
+++ app/src/main/java/com/wsayan/hackster/core/ui/fragment/TopNewsFragment.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
